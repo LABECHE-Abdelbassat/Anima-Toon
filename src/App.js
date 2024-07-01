1051c5c0e2507606070c747d6177e2f73050428a
unknown <labecheabdelbassat@gmail.com>
2024-07-02 00:34:31 +0100

    new

--- src/App.js
+++ src/App.js
@@ -5,49 +5,54 @@ import Landing from "./components/Landing";
 import NavBar from "./components/NavBar";
 import Popular from "./components/Popular";
 import TopRated from "./components/TopRated";
-import "./css/global.css"
-import {Route,Routes , BrowserRouter} from "react-router-dom"
-import  axios  from 'axios';
+import "./css/global.css";
+import { Route, Routes, HashRouter } from "react-router-dom";
+import axios from "axios";
 import ResultPage from "./components/ResultPage";
 
-
 function App() {
+  const [type, setType] = useState("");
 
-  const [type, settype] = useState("")
-  function modifyType(type){
-    settype(type)
+  function modifyType(type) {
+    setType(type);
   }
 
   return (
     <div className="">
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={
-          <div>
-            <NavBar modifyType={modifyType} />
-            <Landing/>
-            <Popular/>
-            <TopRated/>
-            <AllFilms/>
-            </div>
-        }/>
-        <Route path="films" element={
-          <div className="nav-bg">
-            <NavBar modifyType={modifyType} />
-            <ResultPage type={type}/>
-          </div>
-        }/>
-        <Route path="film_details" element={
-          <div>
-            <NavBar modifyType={modifyType}/>
-            <FilmPage/>
-          </div>}/>
-      </Routes>
-    </BrowserRouter>
-     
-
-
+      <HashRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <NavBar modifyType={modifyType} />
+                <Landing />
+                <Popular />
+                <TopRated />
+                <AllFilms />
+              </div>
+            }
+          />
+          <Route
+            path="films"
+            element={
+              <div className="nav-bg">
+                <NavBar modifyType={modifyType} />
+                <ResultPage type={type} />
+              </div>
+            }
+          />
+          <Route
+            path="film_details"
+            element={
+              <div>
+                <NavBar modifyType={modifyType} />
+                <FilmPage />
+              </div>
+            }
+          />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
